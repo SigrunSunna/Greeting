bbe0f15822a282c12c705a514312d234f452ba7a
Sigrun Sunna <sigrunsg16@ru.is>
2018-10-18 16:08:31 +0000

    fullt af doti

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const greeting = require('./src/greeting');
+const express = require("express");
+const app = require("./src/api");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+ console.log("Server running on port " + PORT);
+});
 
-console.log(greeting('Totoro'));
+
+module.exports = app;
